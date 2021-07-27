f386702af383d816330edc2fe02455c399aa456c
Alexandre Lima <alexandreatlima@gmail.com>
2021-07-27 17:52:33 -0300

    global styles

--- src/routeComponents/Home.js
+++ src/routeComponents/Home.js
@@ -1,21 +1,39 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import Logo from "../assets/images/logos/logo.svg";
+import List from "../assets/images/cards-home/list.svg";
+import Star from "../assets/images/cards-home/star.svg";
+import TV from "../assets/images/cards-home/tv.svg";
+
 function Home() {
   return (
-    <section className="text-center">
+    <section>
       <nav>
-        <h1>Spicy Avocado</h1>
-        <img /> <Link to="/auth/login">Ja é cadastrado? Faça login aqui!</Link>
-        <Link className="btn btn-lg btn-primary" to="/auth/signup">
-          Cadastre-se
-        </Link>
+        <img src={Logo} alt="Logo" />
+        <Link to="/auth/login">Ja é cadastrado? Faça login aqui!</Link>
+        <Link to="/auth/signup">Cadastre-se</Link>
       </nav>
       <h2>Só crítica APIMENTADA!</h2>
       <nav>
-        <div><img/><p>Receba <strong>tips</strong> toda semana</p></div>
-        <div><img/><p>Crie suas <strong>Watch Lists!</strong></p></div>
-        <div><img/><p>Poste suas <strong>avaliações!</strong></p></div>
+        <div>
+          <img src={TV} alt="TV" />
+          <p>
+            Receba <strong>tips</strong> toda semana
+          </p>
+        </div>
+        <div>
+          <img src={List} alt="List" />
+          <p>
+            Crie suas <strong>Watch Lists!</strong>
+          </p>
+        </div>
+        <div>
+          <img src={Star} alt="Star" />
+          <p>
+            Poste suas <strong>avaliações!</strong>
+          </p>
+        </div>
       </nav>
     </section>
   );
