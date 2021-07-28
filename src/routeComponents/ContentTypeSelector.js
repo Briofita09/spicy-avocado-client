7c3019a7e62e81df6fe44beaf164a25e474e537d
Alexandre Lima <alexandreatlima@gmail.com>
2021-07-28 18:02:49 -0300

    after help

--- src/routeComponents/ContentTypeSelector.js
+++ src/routeComponents/ContentTypeSelector.js
@@ -1,27 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import listIcon from "../assets/images/navbar/streamline-icon-pencil-write-1@48x48.png";
+import accIcon from "../assets/images/navbar/streamline-icon-single-neutral_1@48x48.png";
+
+import Logo from "../assets/images/logos/horizontal.svg";
+
 export default function ContentTypeSelector() {
   return (
     <section>
       <nav>
-        <img src="../assets/images/logos/horizontal.svg" alt="Logo">
-          <h1>Spicy Avocado</h1>
-        </img>
-
-        <img
-          src="../assets/images/navbar/streamline-icon-pencil-write-1@48x48.png"
-          alt="Icone: Minha Lista"
-        >
-          <Link to="/">Minha Lista</Link>
-        </img>
-
-        <img
-          src="../assets/images/navbar/streamline-icon-house-2@48x48.png"
-          alt="Icone: Perfil"
-        >
-          <Link to="/profile">Minha Conta</Link>
-        </img>
+        <img src={Logo} alt="Logo" />
+        <Link to="/">
+          <img src={listIcon} alt="Icone: Minha Lista" />
+          Minha Lista
+        </Link>
+        <Link to="/profile">
+          <img src={accIcon} alt="Icone: Perfil" />
+          Minha Conta
+        </Link>
       </nav>
       <h3>O que gostaria de pesquisar?</h3>
       <Link to="/">Filmes</Link>
