6edd7907f001620f932fa3ed18b87870571fad7e
Alexandre Lima <alexandreatlima@gmail.com>
2021-07-29 15:44:07 -0300

    estilos do slider

---
+++ src/components/PopularSlider.js
@@ -0,0 +1,75 @@
+import React, { Component } from "react";
+import Slider from "react-slick";
+import style from "../assets/styles/PopularSlider.module.scss";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+export default class SimpleSlider extends Component {
+  state = {
+    contentData: [],
+  };
+  componentDidMount = async () => {
+    const responsive = await axios.get(
+      `https://api.themoviedb.org/3/discover/movie?api_key=1dbc566a4812e099606bf66f83159d6e&language=pt-BR&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate`
+    );
+    this.setState({ contentData: [...responsive.data.results] });
+  };
+
+  render() {
+    const settings = {
+      dots: true,
+      infinite: false,
+      speed: 500,
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      initialSlide: 0,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true,
+          },
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            initialSlide: 2,
+          },
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          },
+        },
+      ],
+    };
+    return (
+      <div className={style.sliderContainer}>
+        <Slider {...settings}>
+          {this.state.contentData.map((content) => {
+            return (
+              <div>
+                <Link to={`/${this.props.params}/${content.id}`}>
+                  <figure>
+                    <img
+                      src={`https://image.tmdb.org/t/p/w200/${content.poster_path}`}
+                      alt={content.original_title}
+                    />
+                    <figcaption>{content.original_title}</figcaption>
+                  </figure>
+                </Link>
+              </div>
+            );
+          })}
+        </Slider>
+      </div>
+    );
+  }
+}
