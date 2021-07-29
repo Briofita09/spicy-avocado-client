918c4f4417334c8d38c62ec48de42e0ce7094543
Alexandre Lima <alexandreatlima@gmail.com>
2021-07-29 19:41:18 -0300

    content screen styled

--- src/routeComponents/ContentDescription.js
+++ src/routeComponents/ContentDescription.js
@@ -2,9 +2,11 @@ import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import api from "../apis/api";
 import axios from "axios";
+import style from "../assets/styles/ContentDescription.module.scss";
 
-// import like from "../assets/images/other-icons/like.png";
-// import dislike from "../assets/images/other-icons/dislike.png";
+import like from "../assets/images/other-icons/like.png";
+import dislike from "../assets/images/other-icons/dislike.png";
+import NavBar from "../components/NavBar";
 
 function ContentDescription() {
   const [tmdbState, setTmdbState] = useState({
@@ -57,61 +59,63 @@ function ContentDescription() {
   console.log(commentState.comments);
 
   return (
-    <div>
-      <section className="topo">
-        <div>
+    <>
+      <NavBar />
+      <section className={style.contentContainer}>
+        <div className={style.picAndTitle}>
           <img
             src={`https://image.tmdb.org/t/p/w200/${tmdbState.poster_path}`}
+            alt={tmdbState.original_title}
           />
-        </div>
-        <div>
-          <h1>{tmdbState.original_title}</h1>
-          <h1>{tmdbState.release_date}</h1>
-        </div>
-        <div>
-          <h3>Gênero: </h3>
-          <ul>
-            {tmdbState.genres.map((genre) => {
-              return <li>{genre.genre}</li>;
-            })}
-          </ul>
-        </div>
-        <div>
-          <h3>Sinopse: </h3>
-          <p>{tmdbState.overview}</p>
-        </div>
-        <div className="likebtn">
-          <button>Bom</button>
-          <span>{likeState.likeCounter}</span>
-        </div>
-        <div className="dislikebtn">
-          <button>Ruim</button>
-          <span>{likeState.dislikeCounter}</span>
-        </div>
-      </section>
-      <hr />
-      <section className="Comentarios">
-        <div>
-          <div>
-            <h1>Discussão:</h1>
-            <ul>
-              {commentState.comments.map((comment) => {
-                return (
-                  <li>
-                    <h4>{comment.commentCreator.name}</h4>
-                    <p>{comment.title}</p>
-                    <span>{comment.comment}</span>
-                  </li>
-                );
-              })}
-            </ul>
+          <div className={style.infos}>
+            <h1>{tmdbState.original_title}</h1>
+            <h1>{` (${new Date(tmdbState.release_date).getFullYear()})`}</h1>
+            <h3>Sinopse: </h3>
+            <p>{tmdbState.overview}</p>
+            <section className={style.rateSection}>
+              <button type="submit">
+                <img src={like} alt="Like Icon" />
+              </button>
+              <button type="submit">
+                <img src={dislike} alt="Dislike Icon" />
+              </button>
+            </section>
           </div>
         </div>
+        <div className={style.genresBlock}>
+          <h3>Gênero: </h3>
+          <Link>
+            {tmdbState.genres.map((genre) => {
+              return (
+                <button type="button" className={style.Button}>
+                  {genre.name}
+                </button>
+              );
+            })}
+          </Link>
+        </div>
+      </section>
+      <hr className={style.sectionDiv} />
+      <section className={style.commentsSection}>
+        <h1>Discussão:</h1>
+        <ul>
+          {commentState.comments.map((comment) => {
+            return (
+              <li>
+                <h4>{comment.commentCreator.name}</h4>
+                <p>{comment.title}</p>
+                <span>{comment.comment}</span>
+              </li>
+            );
+          })}
+        </ul>
       </section>
       <Link to={`/${contentType}/${contentId}/contentComments`}>
-        <button>Comentar na discussão</button>
+        <button type="button" className={style.Button}>
+          Comentar na discussão
+        </button>
       </Link>
-    </div>
+    </>
   );
 }
 
